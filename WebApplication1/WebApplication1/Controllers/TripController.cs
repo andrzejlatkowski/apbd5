0fc7a41f6db4abc9c380fe07d85fc9fb7422d6cb
Andrzej Latkowski <s26442@pjwstk.edu.pl>
2024-05-22 14:51:56 +0200

    AddClientToTrip done

--- WebApplication1/WebApplication1/Controllers/TripController.cs
+++ WebApplication1/WebApplication1/Controllers/TripController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using WebApplication1.Context;
 using WebApplication1.DTOs;
 using WebApplication1.Models;
@@ -73,5 +74,50 @@ public class TripController: ControllerBase
         await _context.SaveChangesAsync();
 
         return Ok();
+    }
+
+    [HttpPost("{idTrip}/clients")]
+    public async Task<IActionResult> AddClientToTrip(int idTrip, ClientTripAssignmentDTO request)
+    {
+        var client = await _context.Clients.FirstOrDefaultAsync(c => c.Pesel == request.Pesel);
+        if (client == null)
+        {
+            client = new Client
+            {
+                IdClient = request.IdClient,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Telephone = request.Telephone,
+                Pesel = request.Pesel,
+            };
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+        }
+        
+        var existingAssignment = await _context.ClientTrips
+            .FirstOrDefaultAsync(ct => ct.IdClient == client.IdClient && ct.IdTrip == idTrip);
+        if (existingAssignment != null)
+        {
+            return BadRequest("Client already assigned to this trip");
+        }
+        
+        var trip = await _context.Trips.FindAsync(idTrip);
+        if (trip == null)
+        {
+            return NotFound("Trip not found");
+        }
+
+        var clientTrip = new ClientTrip()
+        {
+            IdClient = client.IdClient,
+            IdTrip = trip.IdTrip,
+            RegisteredAt = DateTime.Now,
+            PaymentDate = request.PaymentDate,
+        };
+        _context.ClientTrips.Add(clientTrip);
+        await _context.SaveChangesAsync();
+
+        return Ok("Client assigned to the trip");
     }
 }
